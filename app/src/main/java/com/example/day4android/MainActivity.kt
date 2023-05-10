2024db5edddad7cfafba0b85cf2f8c1718e40b02
aalherz <aalherz001@gmail.com>
2023-05-10 20:29:48 +0300

    db helper has been created

--- app/src/main/java/com/example/day4android/MainActivity.kt
+++ app/src/main/java/com/example/day4android/MainActivity.kt
@@ -4,16 +4,18 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
 import com.example.day4android.databinding.ActivityMainBinding
+import com.example.day4android.helper.DbHelper
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        
+        var dbHelper = DbHelper(this)
         
 //        var button = findViewById<>()
         binding.buttonLogin.setOnClickListener {
+            dbHelper.connect()
             Toast.makeText(this, "test", Toast.LENGTH_SHORT).show()
         }
 
