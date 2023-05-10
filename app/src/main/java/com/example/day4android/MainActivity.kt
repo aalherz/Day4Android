c8182ee497b4fe52ec23e1a9b4ad221dac6a64c9
aalherz <aalherz001@gmail.com>
2023-05-10 21:39:31 +0300

    day 4 HW

--- app/src/main/java/com/example/day4android/MainActivity.kt
+++ app/src/main/java/com/example/day4android/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.day4android
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
@@ -15,8 +16,35 @@ class MainActivity : AppCompatActivity() {
         
 //        var button = findViewById<>()
         binding.buttonLogin.setOnClickListener {
-            dbHelper.connect()
+            
+            if(binding.emailET.text.isEmpty()&& binding.editTextPassword.text.isEmpty()){
+                Toast.makeText(this, "لابد من ادخال القيم", Toast.LENGTH_SHORT).show()
+            }else{
+                if(dbHelper.signIn(binding.emailET.text.toString().trim(),binding.editTextPassword.text.toString().trim())){
+                    Toast.makeText(this, "user logged", Toast.LENGTH_SHORT).show()
+                    var i = Intent(this,Home::class.java)
+                    startActivity(i)
+                }else {
+                    Toast.makeText(this, "email or password is wrong", Toast.LENGTH_SHORT).show()
+                }
+            }
+            
             Toast.makeText(this, "test", Toast.LENGTH_SHORT).show()
+        }
+        binding.buttonSign.setOnClickListener {
+            
+            // Todo: create email validation for this button
+        if(binding.emailET.text.isEmpty()&& binding.editTextPassword.text.isEmpty()){
+            Toast.makeText(this, "لابد من ادخال القيم", Toast.LENGTH_SHORT).show()
+        } else{
+            if(dbHelper.createUser(binding.emailET.text.toString().trim(),binding.editTextPassword.text.toString().trim())){
+                Toast.makeText(this, "User has been created", Toast.LENGTH_SHORT).show()
+            }else{
+                Toast.makeText(this, "Somthing went wrong", Toast.LENGTH_SHORT).show()
+            }
+                
+        }
+            
         }
 
     }
--- app/src/main/java/com/example/day4android/helper/DbHelper.kt
+++ app/src/main/java/com/example/day4android/helper/DbHelper.kt
@@ -2,16 +2,16 @@ package com.example.day4android.helper
 
 import android.content.ContentValues
 import android.content.Context
+import android.database.Cursor
 import android.database.sqlite.SQLiteDatabase
 import android.database.sqlite.SQLiteOpenHelper
 import android.util.Log
-import android.widget.Toast
 
 class DbHelper(context:Context) : SQLiteOpenHelper(context, "myDb",null,1) {
     override fun onCreate(db: SQLiteDatabase?) {
-        val qurey = "CREATE TABLE $TABLE_NAME (" +
-                "$USER_ID_COL INTEGER PRIMARY KEY AUTOINCREMENT," +
-                "$USER_EMAIL_COL TEXT," +
+        val qurey = "CREATE TABLE $TABLE_NAME ("+
+                "$USER_ID_COL INTEGER PRIMARY KEY AUTOINCREMENT,"+
+                "$USER_EMAIL_COL TEXT,"+
                 "$USER_PASSWORD_COL TEXT)"
 
         db?.execSQL(qurey)
@@ -21,18 +21,38 @@ class DbHelper(context:Context) : SQLiteOpenHelper(context, "myDb",null,1) {
         Log.d("db","connected")
     }
 
-    override fun onUpgrade(p0: SQLiteDatabase?, p1: Int, p2: Int) {
+    override fun onUpgrade(db: SQLiteDatabase?, p1: Int, p2: Int) {
         TODO("Not yet implemented")
     }
 
-    fun createUser(email:String,password:String){
+    fun createUser(email:String,password:String):Boolean{
         var db = this.writableDatabase
         var cv = ContentValues()
         cv.put(USER_EMAIL_COL,email)
         cv.put(USER_PASSWORD_COL,password)
-        db.insert(TABLE_NAME,null, cv)
+       var result= db.insert(TABLE_NAME,null, cv)
         db.close()
+        return  !result.equals(-1)
     }
+
+    fun signIn(email:String,password:String):Boolean{
+        var db = this.readableDatabase
+        var cols = arrayOf(USER_ID_COL, USER_EMAIL_COL, USER_PASSWORD_COL)
+        var selections = "$USER_EMAIL_COL=? AND $USER_PASSWORD_COL=?"
+        var selcArgs = arrayOf(email,password)
+
+        var cursor :Cursor = db.query(TABLE_NAME, cols,selections, selcArgs,null,null,null)
+
+        var count = cursor.count
+
+        return count != 0
+
+    }
+
+    fun deleteUser(id:String){
+        // todo:  create delete function which delete a user from the database
+    }
+
 
 
     companion object {
